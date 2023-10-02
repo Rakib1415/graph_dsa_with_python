190830318653f956b76bab033eeb02e1d55983aa
Rakib1415 <rakibiuaece@gmail.com>
2023-10-02 19:44:28 +0600

    added graph travarsal algorithm BFS and DFS

--- adjacencyList.py
+++ adjacencyList.py
@@ -1,3 +1,4 @@
+from collections import deque
 graph = {}
 def add_node(v):
     if v in graph:
@@ -5,23 +6,57 @@ def add_node(v):
     else:
         graph[v] = []  
 
-def add_edge(u, v, cost=1):
+def add_edge(u, v):
     if u not in graph and v not in graph:
         print(u, "or", v, "is not present in graph")
     else:
-        graph[u].append([v, cost])
-        graph[v].append([u, cost])    
+        graph[u].append(v)
+        graph[v].append(u)    
+
+# graph traverse algorithm Depth First Search
+def DFS(node, graph):
+    if node not in graph:
+        print("Node is not present in graph")
+        return
+    visited = set()
+    stack = []
+    stack.append(node)
+    while stack:
+        current = stack.pop()
+        if current not in visited:
+            print(current)
+            visited.add(current)
+            for i in graph[current]:
+                stack.append(i)            
+
+# graph traverse algorithm Breath First Search
+def BFS(node, graph):
+    if node not in graph:
+        print("Node is not present in graph")
+        return
+    visited = set()
+    myqueue = deque()
+    myqueue.append(node)
+    visited.add(node)
+    while myqueue:
+        current = myqueue.popleft()
+        print(current)
+        for itemNode in graph[current]:
+            if not itemNode in visited:
+                visited.add(itemNode)
+                myqueue.append(itemNode)
+
 
 add_node("A")
 add_node("B")
 add_node("C")
 add_node("D")
 add_node("E")
-add_edge("A", "B", 3)
-add_edge("A", "C", 4)
-add_edge("A", "D", 7)
+add_edge("A", "B")
+add_edge("A", "C")
+add_edge("A", "D")
 add_edge("B", "E")
 add_edge("B", "D")
 add_edge("C", "D")
-add_edge("D", "E", 5)
-print(graph)
+add_edge("D", "E")
+BFS("A", graph)
